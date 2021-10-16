7dc70e098522ba8c800dfe460684e7c9b7518d7f
Yasin Javar <yasinjjjv@gmail.com>
2021-10-16 14:21:19 +0330

    test

--- encoder.py
+++ encoder.py
@@ -6,6 +6,6 @@ sample_string_bytes = text.read().encode("ascii")
 
 base64_bytes = base64.b64encode(sample_string_bytes)
 base64_string = base64_bytes.decode("ascii")
-print(base64_string)
+# print(base64_string)
 with open('SS','w') as f:
     f.write(base64_string)
