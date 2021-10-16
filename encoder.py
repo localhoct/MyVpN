c65e7e6d9d0fe769707ccd9ae5649daecc5c3498
Yasin Javar <yasinjjjv@gmail.com>
2021-10-16 14:19:30 +0330

    test

---
+++ encoder.py
@@ -0,0 +1,11 @@
+import base64
+
+text = open('ServerForDecode.txt','r')
+# print(text.read())
+sample_string_bytes = text.read().encode("ascii")
+
+base64_bytes = base64.b64encode(sample_string_bytes)
+base64_string = base64_bytes.decode("ascii")
+print(base64_string)
+with open('SS','w') as f:
+    f.write(base64_string)
